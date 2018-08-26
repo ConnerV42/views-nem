1b77a1b07ebbc5ee9b6d67876fa0aebdeaf35096
ConnerV42 <connerverret42@gmail.com>
2018-08-26 20:50:57 +0000

    Views Version 3

---
+++ v3/app.js
@@ -0,0 +1,90 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    View = require("./models/view"),
+    seedDB = require("./seeds");
+
+seedDB();
+mongoose.connect("mongodb://localhost/viewsdb");
+app.use(bodyParser.urlencoded({extended: true})); 
+app.set("view engine", "ejs");
+
+var viewsArray = [
+    
+        {name: "Downtown Spokane River", image: "https://farm5.staticflickr.com/4461/38066017492_a17699dd5a.jpg"},
+        {name: "Downtown Spokane", image: "https://farm8.staticflickr.com/7438/27204094683_06b593ef33.jpg"},
+        {name: "Iron Bridge", image: "https://farm9.staticflickr.com/8710/17196166358_3b6ebf44c7.jpg"},
+        {name: "Near Parkade Plaza", image: "https://farm5.staticflickr.com/4428/36662132000_b59dd5572f.jpg"},
+        {name: "Maple Street Bridge", image: "https://farm8.staticflickr.com/7404/12369917523_3091aca308.jpg"},
+        {name: "Riverfront Park Spokane", image: "https://farm5.staticflickr.com/4445/37386351264_c32f2b194a.jpg"},
+        {name: "Spokane Riverbank", image: "https://farm4.staticflickr.com/3675/9416605560_a0500495b6.jpg"},
+        {name: "Post Bridge", image: "https://farm9.staticflickr.com/8751/17198652918_fd5b0999ec.jpg"},
+        {name: "Sunrise", image: "https://farm5.staticflickr.com/4480/37393916474_bb91898648.jpg"},
+        {name: "Downtown Spokane", image: "https://theculturetrip.com/wp-content/uploads/2015/11/2309029891_725f89a507_b.jpg"},
+        {name: "Clocktower", image: "https://api.trekaroo.com//photos/flickr_cache/46735/46735.jpg"},
+        {name: "Riverfront Park in the Winter", image: "http://scelnews.com.s3.amazonaws.com/wp-content/uploads/2014/01/river-front-park-spokane.jpg"},
+        {name: "Gondolas Above Spokane River", image: "http://www.historylink.org/Content/Media/Photos/Large/Spokane_Falls_6-30-2014.jpg"},
+        {name: "Forest", image: "https://s3.amazonaws.com/iexplore_web/images/assets/000/001/383/original/Spokane-Kidron_Cool.jpg?1435957459"},
+        {name: "Trent and Hamilton", image: "https://www.aaroads.com/west/washington090/i-090_eb_exit_282_02.jpg"},
+        {name: "Spokane River Flowing", image:"https://farm9.staticflickr.com/8588/16431873628_c70b19b22e_b.jpg"},
+        {name: "2 Main Features", image:"http://farm4.staticflickr.com/3588/3505956193_e22131c8fc_b.jpg"},
+        {name: "McCain and Obama Bicken", image: "https://c1.staticflickr.com/4/3241/2876390035_503e3976c3_b.jpg"}
+]
+
+
+app.get("/", function(req, res){
+   res.render("landing"); 
+});
+
+//INDEX - Show all views
+app.get("/views", function(req, res){
+    // Get all views from DB
+    View.find({}, function(err, allViews){
+       if(err) {
+           console.log(err);
+       } else {
+           res.render("index", {views:allViews});
+       }
+    });
+});
+
+//CREATE ROUTE - Add new view to database
+app.post("/views", function(req, res){
+   //get data from the form and add to views database
+   var name = req.body.name;
+   var image = req.body.image;
+   var desc = req.body.description;
+   var newView = {name: name, image: image, description: desc};
+   //Create new campground and save to DB
+   View.create(newView, function(err, newlyCreated){
+        if(err) {
+            console.log(err);
+        } else {
+            //redirect back to the viewsPage
+            res.redirect("/views");
+        }
+   });
+});
+
+//NEW ROUTE - show form to create new view
+app.get("/views/new", function(req, res){
+   res.render("new.ejs"); 
+});
+
+// SHOW ROUTE - shows more info about one view 
+app.get("/views/:id", function(req, res){
+    //find the campground with the provided id
+    View.findById(req.params.id).populate("comments").exec( function(err, foundView){
+       if(err){
+           console.log(err);
+       } else {
+           console.log(foundView);
+           res.render("show", {view: foundView});
+       }
+    });
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+   console.log("Views Server has started!"); 
+});
---
+++ v3/models/view.js
@@ -0,0 +1,18 @@
+//SCHEMA SETUP for MONGOOSE
+var mongoose = require("mongoose");
+
+var viewSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+   description: String,
+   comments: [ //Here we are building a reference to comment id's by association, hence the ref tag that is labeled, "Comment"
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
+});
+
+var View = mongoose.model("View", viewSchema);
+
+module.exports = mongoose.model("View", viewSchemaclear
